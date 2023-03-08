4fad81b436f481e05f94efdfcf4fb40ddedc712e
Deepanshu0810 <deepanshu0810@gmail.com>
2023-03-08 15:29:32 +0530

    SETUP: jinja-templates

--- website/auth.py
+++ website/auth.py
@@ -1,15 +1,15 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 auth = Blueprint('auth',__name__)
 
 @auth.route('/login')
 def login():
-    return "<h1>Login</h1>"
+    return render_template("login.html")
 
 @auth.route('/logout')
 def logout():
     return "<h1>Logout</h1>"
 
-@auth.route('/sign-up')
+@auth.route('/signup')
 def signup():
-    return "<h1>Sign-Up</h1>"
+    return render_template("signup.html")

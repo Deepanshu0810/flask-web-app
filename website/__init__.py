d35433ce3dbe726ee0fbf1ce46398d14a6665643
Deepanshu0810 <deepanshu0810@gmail.com>
2023-03-15 23:29:36 +0530

    ADD: login-logout

--- website/__init__.py
+++ website/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import os
+from flask_login import LoginManager
 
 db=SQLAlchemy()
 DB_NAME = "database.db"
@@ -12,15 +13,26 @@ def create_app():
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+os.path.join(basedir,DB_NAME)
     db.init_app(app)
 
+    
+
     from .views import views
     from .auth import auth
     
 
     app.register_blueprint(views,url_prefix='/')
     app.register_blueprint(auth,url_prefix='/auth/') #any route inside the auth will have the prefix /auth/
+    
     from .models import User,Note
-
     create_db(app)
+
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
+
     return app
 
 def create_db(app):

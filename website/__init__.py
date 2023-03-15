a1d8e7c11c0fe587af89bc142e2c8eb65ef635dc
Deepanshu0810 <deepanshu0810@gmail.com>
2023-03-15 20:56:31 +0530

    ADD: user-creation

--- website/__init__.py
+++ website/__init__.py
@@ -1,20 +1,30 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+import os
 
 db=SQLAlchemy()
 DB_NAME = "database.db"
-
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'helloworld_secretkey'
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+os.path.join(basedir,DB_NAME)
     db.init_app(app)
 
     from .views import views
     from .auth import auth
+    
 
     app.register_blueprint(views,url_prefix='/')
     app.register_blueprint(auth,url_prefix='/auth/') #any route inside the auth will have the prefix /auth/
+    from .models import User,Note
+
+    create_db(app)
     return app
 
+def create_db(app):
+    with app.app_context():
+        if not os.path.exists('website/'+DB_NAME):
+            db.create_all()
+            print('database created!')
--- website/models.py
+++ website/models.py
@@ -2,10 +2,11 @@ from . import db
 from flask_login import UserMixin
 from sqlalchemy.sql import func
 
+
 class Note(db.Model):
     id=db.Column(db.Integer,primary_key=True)
     data = db.Column(db.String(10000))
-    date = db.Column(db.DateTime(timezone=True),defaul=func.now())
+    date = db.Column(db.DateTime(timezone=True),default=func.now())
 
     # associate note with aa user / relationship b/w noe and user
     user_id = db.Column(db.Integer,db.ForeignKey('user.id'))
@@ -16,3 +17,4 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(100),unique=True)
     password = db.Column(db.String(100))
     name = db.Column(db.String(100))
+    notes = db.relationship('Note')
